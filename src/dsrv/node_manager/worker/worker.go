365e82b2eecb52edcf40ff1632b501c287166551
Dong Yoo Kang <xellos.kang@gmail.com>
2022-02-28 21:51:50 -0600

    New Structure for Vatz-Node Managers

--- src/dsrv/node_manager/worker/worker.go
+++ src/dsrv/node_manager/worker/worker.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"context"
+	managerpb "github.com/xellos00/silver-bentonville/dist/proto/dsrv/api/node_manager/v1"
 	"log"
 	"time"
 )
@@ -10,16 +12,16 @@ const (
 	defaultChannelBufferSize = 10
 )
 
-type managerWorker struct {
+type ManagerWorker struct {
 	CheckInterval time.Duration
 	workerChannel chan bool
 }
 
-func (p *managerWorker) Start() error {
+func (p *ManagerWorker) Init() error {
 	return nil
 }
 
-func (p *managerWorker) Execute() error {
+func (p *ManagerWorker) Execute() error {
 
 	if p.CheckInterval <= 0 {
 		p.CheckInterval = defaultCheckInterval
@@ -33,19 +35,27 @@ func (p *managerWorker) Execute() error {
 	return nil
 }
 
-func (p *managerWorker) Stop() error {
+func (p *ManagerWorker) Verify(ctx context.Context, in *managerpb.VerifyRequest) (*managerpb.VerifyInfo, error) {
+	return nil, nil
+}
+
+func (p *ManagerWorker) UpdateConfig(ctx context.Context, in *managerpb.UpdateRequest) (*managerpb.UpdateResponse, error) {
+	return nil, nil
+}
+
+func (p *ManagerWorker) End() error {
 	return nil
 }
 
-func (p *managerWorker) worker(req <-chan bool) {
+func (p *ManagerWorker) worker(req <-chan bool) {
 	for {
 		_ = <-req
 		log.Println("Do something")
-		//TODO: Excute to call another plugin
+		//TODO: Execute to call another plugin
 	}
 }
 
-func (p *managerWorker) invoker(c chan<- bool) {
+func (p *ManagerWorker) invoker(c chan<- bool) {
 	for {
 		c <- true
 		time.Sleep(p.CheckInterval)
--- src/main.go
+++ src/main.go
@@ -6,6 +6,8 @@ import (
 	managerpb "github.com/xellos00/silver-bentonville/dist/proto/dsrv/api/node_manager/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -18,8 +20,28 @@ const (
 )
 
 var (
-	grpcPort = 9090
+	defaultConf = getConf()
 )
+
+func getConf() map[interface{}]interface{} {
+	wd, _ := os.Getwd()
+	confPath := fmt.Sprintf("%s/src/dsrv/conf/default.yaml", wd)
+
+	yamlFile, err := ioutil.ReadFile(confPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make(map[interface{}]interface{})
+	err2 := yaml.Unmarshal(yamlFile, &data)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	return data
+}
 
 func preLoad() error {
 	//TODOs: Check the Configs and return dict as global variable.
@@ -27,6 +49,7 @@ func preLoad() error {
 }
 
 func initiateServer(ch <-chan os.Signal) error {
+
 	log.Println("Initialize Servers:", serviceName)
 	_, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -36,7 +59,7 @@ func initiateServer(ch <-chan os.Signal) error {
 
 	managerpb.RegisterNodeManagerServer(s, &serv)
 	reflection.Register(s)
-
+	grpcPort := defaultConf["port"]
 	addr := fmt.Sprintf(":%d", grpcPort)
 
 	listener, err := net.Listen("tcp", addr)
@@ -66,6 +89,7 @@ func initiateServer(ch <-chan os.Signal) error {
 }
 
 func main() {
+	preLoad()
 	ch := make(chan os.Signal, 1)
 	initiateServer(ch)
 }
