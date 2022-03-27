42d18f1ffd7c35f8bf143496a3bc559497a84d42
Gnong <jinhongdev@gmail.com>
2022-03-27 14:49:15 +0900

    feature: Create discord alert

--- alert/discord.go
+++ alert/discord.go
@@ -1,4 +1,4 @@
-package main
+package alert
 
 import (
 	"bytes"
@@ -6,38 +6,6 @@ import (
 	"fmt"
 	"net/http"
 )
-
-type discordMsg struct {
-	Username  string `json:"username,omitempty"`
-	AvatarURL string `json:"avatar_url,omitempty"`
-	Content   string `json:"content,omitempty"`
-	Embeds    []struct {
-		Author struct {
-			Name    string `json:"name,omitempty"`
-			URL     string `json:"url,omitempty"`
-			IconURL string `json:"icon_url,omitempty"`
-		} `json:"author,omitempty"`
-		Title       string `json:"title"`
-		URL         string `json:"url,omitempty"`
-		Description string `json:"description"`
-		Color       int    `json:"color"`
-		Fields      []struct {
-			Name   string `json:"name,omitempty"`
-			Value  string `json:"value,omitempty"`
-			Inline bool   `json:"inline,omitempty"`
-		} `json:"fields,omitempty"`
-		Thumbnail struct {
-			URL string `json:"url,omitempty"`
-		} `json:"thumbnail,omitempty"`
-		Image struct {
-			URL string `json:"ur,omitemptyl"`
-		} `json:"image,omitempty"`
-		Footer struct {
-			Text    string `json:"text,omitempty"`
-			IconURL string `json:"icon_url,omitempty"`
-		} `json:"foote,omitemptyr"`
-	} `json:"embeds"`
-}
 
 type reqMsg struct {
 	FuncName     string `json:"func_name"`
@@ -47,52 +15,89 @@ type reqMsg struct {
 	ResourceType string `json:"resource_type"`
 }
 
+type embed struct {
+	Author struct {
+		Name    string `json:"name,omitempty"`
+		URL     string `json:"url,omitempty"`
+		IconURL string `json:"icon_url,omitempty"`
+	} `json:"author,omitempty"`
+	Title       string `json:"title"`
+	URL         string `json:"url,omitempty"`
+	Description string `json:"description"`
+	Color       int    `json:"color"`
+	Fields      []struct {
+		Name   string `json:"name,omitempty"`
+		Value  string `json:"value,omitempty"`
+		Inline bool   `json:"inline,omitempty"`
+	} `json:"fields,omitempty"`
+	Thumbnail struct {
+		URL string `json:"url,omitempty"`
+	} `json:"thumbnail,omitempty"`
+	Image struct {
+		URL string `json:"url,omitempty"`
+	} `json:"image,omitempty"`
+	Footer struct {
+		Text    string `json:"text,omitempty"`
+		IconURL string `json:"icon_url,omitempty"`
+	} `json:"footer,omitempty"`
+}
+
+type discordMsg struct {
+	Username  string  `json:"username,omitempty"`
+	AvatarURL string  `json:"avatar_url,omitempty"`
+	Content   string  `json:"content,omitempty"`
+	Embeds    []embed `json:"embeds"`
+}
+
+const (
+	Fail    = "FAIL"
+	Success = "SUCCESS"
+)
+
 var (
 	webhookURL string
-	rMsg       reqMsg
-	sMsg       discordMsg
-
-	format      string
-	failRequest string
-	succRequest string
 )
 
 func init() {
 	webhookURL = "https://discord.com/api/webhooks/955326233057566750/e-Tkq6mvxXYsx3Rqi3ttxDes76UXPpgk76Bnz5jF8_DwWgdNF-iNf7ZWdqi1nQnHO-zC"
-	format = `{"embeds": [{}]}`
 }
 
 func discord(request string) bool {
-	message := makeForm(failRequest)
+	message := makeForm(request)
 	sendMsg(message)
 }
 
 func makeForm(request string) string {
+	rMsg := reqMsg{}
 	err := json.Unmarshal([]byte(request), &rMsg)
 	if err != nil {
-		fmt.Println("[ERR] format is incorrect")
+		fmt.Println("ERROR | Format is incorrect")
+		panic(err)
 	}
 
-	if rMsg.State == "FAIL" {
-		json.Unmarshal([]byte(format), &sMsg)
+	sMsg := discordMsg{Embeds: make([]embed, 1)}
+	if rMsg.State == Fail {
+		fmt.Println("INFO | Got a fail status")
 		sMsg.Embeds[0].Title = rMsg.Severity
 		sMsg.Embeds[0].Description = rMsg.Msg
 		sMsg.Embeds[0].Color = 15258703
-	} else if rMsg.State == "SUCCESS" {
-		fmt.Println("SUCCESS")
+		fmt.Println("INFO | Create fail message")
+	} else if rMsg.State == Success {
+		fmt.Println("INFO | Got a success status")
 	}
 
-	sendMsg, _ := json.Marshal(sMsg)
-	return string(sendMsg)
+	message, _ := json.Marshal(sMsg)
+	return string(message)
 }
 
-func sendMsg(msg string) {
-	req, _ := http.NewRequest("POST", webhookURL, bytes.NewBufferString(msg))
+func sendMsg(message string) {
+	req, _ := http.NewRequest("POST", webhookURL, bytes.NewBufferString(message))
 	req.Header.Set("Content-Type", "application/json")
 	c := &http.Client{}
 	_, err := c.Do(req)
 	if err != nil {
+		fmt.Println("ERROR | Failed to send discord message")
 		panic(err)
 	}
-	fmt.Println("SEND SUCCESS")
+	fmt.Println("INFO | Send a discord")
 }
