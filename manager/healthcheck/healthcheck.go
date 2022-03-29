839129b97a04236040c07809960f514f2d01a9bd
hwangjae lee <meetrick@gmail.com>
2022-03-29 22:36:20 +0900

    Verifying call to plugin
    Signed-off-by: hwangjae lee <meetrick@gmail.com>

--- manager/healthcheck/healthcheck.go
+++ manager/healthcheck/healthcheck.go
@@ -2,6 +2,9 @@ package healthcheck
 
 import (
 	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
 )
 
 type healthCheck struct {
@@ -10,6 +13,19 @@ type healthCheck struct {
 func (h healthCheck) HealthCheck() (string, error) {
 	fmt.Println("this is sending Notification")
 	//TODO: Set Client to Plugin
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("localhost:9091", opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cc.Close()
+
+	var client = pluginpb.NewPluginClient(cc)
+	var empty = make(map[interface{}]interface{})
+	verify, err := client.Verify(empty)
+	if err != nil {
+		return "", err
+	}
 	return "UP", nil
 }
 
