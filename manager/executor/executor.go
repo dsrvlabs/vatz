ebffdc0f53065ee0b4aedae89de9714f5d71eda6
HeeJin Lee <heejin.lee@dsrvlabs.com>
2022-03-30 08:05:40 +0000

    Issue dsrvlabs#31: Request execute call to Plugin by plugin execute API
    initial commit for execute plugin

--- manager/executor/executor.go
+++ manager/executor/executor.go
@@ -1,7 +1,9 @@
 package executor
 
 import (
+	"log"
 	"context"
+	"google.golang.org/grpc"
 	pluginpb "github.com/xellos00/dk-yuba-proto/dist/proto/vatz/plugin/v1"
 )
 
@@ -13,6 +15,25 @@ type Executor interface {
 }
 
 func (v executor) Execute(ctx context.Context, in *pluginpb.ExecuteRequest) (*pluginpb.ExecuteResponse, error) {
+	log.Printf("executor call plugin")
+
+	opts := grpc.WithInsecure()
+	//TODO: get addr from in.GetExecuteInfo()
+	var address = "localhost:9091"
+	conn, err := grpc.Dial(address, opts)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pluginpb.NewPluginClient(conn)
+
+	//TODO: execute request to plugin
+	resp, _ := client.Execute(context.Background(), in)
+
+	//TODO: handle response from plugin
+	//alert by resp.state, resp.severity
+	log.Printf("received state :%v", resp.GetState())
 	return nil, nil
 }
 
