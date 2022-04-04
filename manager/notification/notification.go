94884ecb62194dc684abb704d4e4fed91581a933
Gnong <jinhongdev@gmail.com>
2022-04-04 10:16:38 +0900

    Send a Notification to Discord

--- manager/notification/notification.go
+++ manager/notification/notification.go
@@ -1,6 +1,11 @@
 package notification
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type notification struct {
 	//duration time.Duration
@@ -8,12 +13,58 @@ type notification struct {
 	//message string
 }
 
-type Notification interface {
-	SendNotification() error
+type embed struct {
+	Author struct {
+		Name    string `json:"name,omitempty"`
+		URL     string `json:"url,omitempty"`
+		IconURL string `json:"icon_url,omitempty"`
+	} `json:"author,omitempty"`
+	Title       string `json:"title"`
+	URL         string `json:"url,omitempty"`
+	Description string `json:"description"`
+	Color       int    `json:"color"`
+	Fields      []struct {
+		Name   string `json:"name,omitempty"`
+		Value  string `json:"value,omitempty"`
+		Inline bool   `json:"inline,omitempty"`
+	} `json:"fields,omitempty"`
+	Thumbnail struct {
+		URL string `json:"url,omitempty"`
+	} `json:"thumbnail,omitempty"`
+	Image struct {
+		URL string `json:"url,omitempty"`
+	} `json:"image,omitempty"`
+	Footer struct {
+		Text    string `json:"text,omitempty"`
+		IconURL string `json:"icon_url,omitempty"`
+	} `json:"footer,omitempty"`
 }
 
-func (d notification) SendNotification() error {
-	fmt.Println("this is sending Notification")
+type discordMsg struct {
+	Username  string  `json:"username,omitempty"`
+	AvatarURL string  `json:"avatar_url,omitempty"`
+	Content   string  `json:"content,omitempty"`
+	Embeds    []embed `json:"embeds"`
+}
+
+type Notification interface {
+	SendDiscord(msg ReqMsg, webhook string) error
+}
+
+func (d notification) SendDiscord(msg ReqMsg, webhook string) error {
+	sMsg := discordMsg{Embeds: make([]embed, 1)}
+	sMsg.Embeds[0].Title = msg.Severity
+	sMsg.Embeds[0].Description = msg.Msg
+	sMsg.Embeds[0].Color = 15258703
+	message, _ := json.Marshal(sMsg)
+	req, _ := http.NewRequest("POST", webhook, bytes.NewBufferString(string(message)))
+	req.Header.Set("Content-Type", "application/json")
+	c := &http.Client{}
+	_, err := c.Do(req)
+	if err != nil {
+		fmt.Println("ERROR | Failed to send discord message")
+		panic(err)
+	}
 	return nil
 }
 
