257d06c5fe4a65532c8d468257b9e7b861ce687c
Dong Yoo Kang <xellos.kang@gmail.com>
2022-04-14 20:42:02 -0500

    call all method into executor.go

--- manager/executor/executor_manager.go
+++ manager/executor/executor_manager.go
@@ -1,11 +1,9 @@
 package executor
 
 import (
-	"context"
 	pluginpb "github.com/xellos00/dk-yuba-proto/dist/proto/vatz/plugin/v1"
 	"google.golang.org/protobuf/types/known/structpb"
 	"log"
-	message "vatz/manager/model"
 	"vatz/manager/notification"
 )
 
@@ -16,6 +14,7 @@ var (
 )
 
 func init() {
+	executorInstance = NewExecutor()
 }
 
 type executorManager struct {
@@ -68,61 +67,18 @@ func (s *executorManager) Execute(gClient pluginpb.PluginClient, pluginInfo inte
 			Options:     options,
 		}
 
-		// There's invalid memory trouble
-		// panic: runtime error: invalid memory address or nil pointer dereference
-		// [signal SIGSEGV: segmentation violation code=0x1 addr=0x18 pc=0x1445d4c]
-		//resp, err := executorInstance.Execute(gClient, req)
-
-		resp, exeErr := gClient.Execute(context.Background(), req)
-
-		if exeErr != nil || resp == nil {
-			resp = &pluginpb.ExecuteResponse{
-				State:     pluginpb.State_FAILURE,
-				Message:   "API Execution Failed",
-				AlertType: pluginpb.ALERT_TYPE_DISCORD,
-				Severity:  pluginpb.Severity_ERROR,
-			}
-		}
+		resp, err := executorInstance.Execute(gClient, req)
 
 		EManager.updateStatus(resp, methodName, exeStatus)
-		notifyInfo := make(map[interface{}]interface{})
 
+		notifyInfo := make(map[interface{}]interface{})
 		notifyInfo["severity"] = resp.GetSeverity().String()
 		notifyInfo["state"] = resp.GetState().String()
 		notifyInfo["method_name"] = methodName
 		notifyInfo["execute_message"] = resp.GetMessage()
 		notifyInfo["plugin_name"] = defaultPluginName
 
-		EManager.ExecuteNotify(notifyInfo, exeStatus)
-		//executorInstance.ExecuteNotify(notifyInfo, exeStatus)
-
+		executorInstance.ExecuteNotify(notifyInfo, exeStatus)
 	}
-
 	return exeStatus
-}
-
-func (s *executorManager) ExecuteNotify(notifyInfo map[interface{}]interface{}, exeStatus map[interface{}]interface{}) error {
-
-	// if response's state is not `SUCCESS` and then we consider all execute call has failed.
-	if notifyInfo["state"] != string(message.Success) {
-		exeStatus[notifyInfo["method_name"]] = false
-
-		if notifyInfo["severity"] == string(message.Error) {
-			jsonMessage := message.ReqMsg{FuncName: notifyInfo["method_name"].(string), State: message.Faliure, Msg: "No response from Plugin", Severity: message.Critical, ResourceType: notifyInfo["plugin_name"].(string)}
-			dispatchManager.SendNotification(jsonMessage)
-		}
-
-		if notifyInfo["severity"] == string(message.Critical) {
-			jsonMessage := message.ReqMsg{FuncName: notifyInfo["method_name"].(string), State: message.Faliure, Msg: notifyInfo["execute_message"].(string), Severity: message.Critical, ResourceType: notifyInfo["plugin_name"].(string)}
-			dispatchManager.SendNotification(jsonMessage)
-		}
-
-	} else {
-		if exeStatus[notifyInfo["method_name"]] == false {
-			jsonMessage := message.ReqMsg{FuncName: notifyInfo["method_name"].(string), State: message.Success, Msg: notifyInfo["execute_message"].(string), Severity: message.Info, ResourceType: notifyInfo["plugin_name"].(string)}
-			dispatchManager.SendNotification(jsonMessage)
-		}
-	}
-
-	return nil
 }
