cd6a68128d89f8bf9781e5cd369b579c6044d069
Gnong <jinhongdev@gmail.com>
2022-05-11 18:25:11 +0900

    Solved conflict

--- manager/notification/notification.go
+++ manager/notification/notification.go
@@ -3,8 +3,9 @@ package notification
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	pluginpb "github.com/dsrvlabs/vatz-proto/plugin/v1"
@@ -21,6 +22,8 @@ const (
 	discordYellow DiscordColor = 16705372
 	discordGreen  DiscordColor = 5763719
 	discordGray   DiscordColor = 9807270
+
+	discordWebhookFormat string = "https://discord.com/api/webhooks/"
 )
 
 type field struct {
@@ -77,29 +80,31 @@ func (d notification) GetNotifyInfo(response *pluginpb.ExecuteResponse, pluginNa
 }
 
 func (d notification) SendDiscord(msg message.ReqMsg, webhook string) error {
-	sMsg := discordMsg{Embeds: make([]embed, 1)}
-	switch msg.Severity {
-	case message.Critical:
-		sMsg.Embeds[0].Color = discordRed
-	case message.Warning:
-		sMsg.Embeds[0].Color = discordYellow
-	case message.Ok:
-		sMsg.Embeds[0].Color = discordGreen
-	default:
-		sMsg.Embeds[0].Color = discordGray
-	}
-	sMsg.Embeds[0].Title = string(msg.Severity)
-	sMsg.Embeds[0].Fields = []field{{Name: msg.ResourceType, Value: msg.Msg, Inline: false}}
-	sMsg.Embeds[0].Timestamp = time.Now()
-
-	message, _ := json.Marshal(sMsg)
-	req, _ := http.NewRequest("POST", webhook, bytes.NewBufferString(string(message)))
-	req.Header.Set("Content-Type", "application/json")
-	c := &http.Client{}
-	_, err := c.Do(req)
-	if err != nil {
-		fmt.Println("ERROR | Failed to send discord message")
-		//panic(err)
+	if strings.Contains(webhook, discordWebhookFormat) {
+		sMsg := discordMsg{Embeds: make([]embed, 1)}
+		switch msg.Severity {
+		case message.Critical:
+			sMsg.Embeds[0].Color = discordRed
+		case message.Warning:
+			sMsg.Embeds[0].Color = discordYellow
+		case message.Ok:
+			sMsg.Embeds[0].Color = discordGreen
+		default:
+			sMsg.Embeds[0].Color = discordGray
+		}
+		sMsg.Embeds[0].Title = string(msg.Severity)
+		sMsg.Embeds[0].Fields = []field{{Name: msg.ResourceType, Value: msg.Msg, Inline: false}}
+		sMsg.Embeds[0].Timestamp = time.Now()
+		message, _ := json.Marshal(sMsg)
+		req, _ := http.NewRequest("POST", webhook, bytes.NewBufferString(string(message)))
+		req.Header.Set("Content-Type", "application/json")
+		c := &http.Client{}
+		_, err := c.Do(req)
+		if err != nil {
+			log.Println("ERROR | Failed to send discord message")
+		}
+	} else {
+		log.Println("ERROR | Invalid discord webhook address")
 	}
 	return nil
 }
