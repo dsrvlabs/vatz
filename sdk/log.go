0e20c9753be7438272d8844f36f7ea6e759c1b9e
Joonkyo Kim <rootwarp@gmail.com>
2022-06-03 07:22:19 +0000

    Apply zerolog package

---
+++ sdk/log.go
@@ -0,0 +1,13 @@
+package sdk
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+}
