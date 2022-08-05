a41eb8727738f36a08fac4e7acdb4965574d6fc5
Joonkyo Kim <rootwarp@gmail.com>
2022-08-05 11:26:21 +0900

    Add init subcommand. (#214)
    * Add init subcommand.
    * Fix template, removing first empty line.

---
+++ cmd_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitCmd(t *testing.T) {
+	tests := []struct {
+		Desc       string
+		Args       []string
+		ExpectFile string
+	}{
+		{
+			Desc:       "Init with default",
+			Args:       []string{"init"},
+			ExpectFile: "default.yaml",
+		},
+		{
+			Desc:       "Init with selected filename",
+			Args:       []string{"init", "--output", "hello.yaml"},
+			ExpectFile: "hello.yaml",
+		},
+	}
+
+	for _, test := range tests {
+		root := cobra.Command{}
+		root.AddCommand(createInitCommand())
+		root.SetArgs(test.Args)
+
+		err := root.Execute()
+		defer os.Remove(test.ExpectFile)
+
+		assert.Nil(t, err)
+
+		_, err = os.ReadFile(test.ExpectFile)
+		assert.Nil(t, err)
+	}
+}
