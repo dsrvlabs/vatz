2dc81303b741fc1908ce8064eee4b99107c97c65
Joonkyo Kim <rootwarp@gmail.com>
2022-09-20 04:41:33 +0900

    Start RPC service. (#247)
    * Start RPC service.
    
    * Add RPC doc.

---
+++ docs/api.md
@@ -0,0 +1,12 @@
+# VATZ API Specs
+
+RPC service is available for managing VATZ. For now, this RPC serive is only available on local.
+
+## Get Plugin's status
+
+Querying plugin's status.
+
+```
+~$ curl localhost:19091/v1/plugin_status
+{"status":"OK","pluginStatus":[{"status":"FAIL","pluginName":"up check"}]}
+```
--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,13 +1,23 @@
 package rpc
 
 import (
+	"context"
 	"net"
+	"net/http"
 
 	vatzpb "github.com/dsrvlabs/vatz-proto/rpc/v1"
 	"github.com/dsrvlabs/vatz/manager/healthcheck"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
+)
+
+var (
+	// TODO: Address should be configurable.
+	grpcAddress = "localhost:19090"
+	httpAddress = "localhost:19091"
 )
 
 // VatzRPC provides RPC interfaces.
@@ -17,34 +27,102 @@ type VatzRPC interface {
 }
 
 type rpcService struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
 	vatzRPCService vatzpb.VatzRPCServer
 	grpcServer     *grpc.Server
+	httpServer     *http.Server
 }
 
 func (s *rpcService) Start() error {
 	log.Info().Str("module", "rpc").Msg("start rpc server")
 
-	l, err := net.Listen("tcp", "127.0.0.1:19090")
-	if err != nil {
-		return err
-	}
+	errChan := make(chan error, 2)
 
-	s.grpcServer = grpc.NewServer()
-	s.vatzRPCService = &grpcService{
-		healthChecker: healthcheck.GetHealthChecker(),
-	}
+	go func(errChan chan<- error) {
+		log.Info().Str("module", "rpc").Msg("start gRPC server")
 
-	vatzpb.RegisterVatzRPCServer(s.grpcServer, s.vatzRPCService)
-	reflection.Register(s.grpcServer)
+		l, err := net.Listen("tcp", grpcAddress)
+		if err != nil {
+			log.Info().Str("module", "rpc").Err(err)
+			errChan <- err
+			return
+		}
 
-	return s.grpcServer.Serve(l)
+		s.grpcServer = grpc.NewServer()
+		s.vatzRPCService = &grpcService{
+			healthChecker: healthcheck.GetHealthChecker(),
+		}
+
+		vatzpb.RegisterVatzRPCServer(s.grpcServer, s.vatzRPCService)
+		reflection.Register(s.grpcServer)
+
+		err = s.grpcServer.Serve(l)
+		if err != nil {
+			log.Info().Str("module", "rpc").Err(err)
+			errChan <- err
+			return
+		}
+	}(errChan)
+
+	go func(errChan chan<- error) {
+		log.Info().Str("module", "rpc").Msg("start gRPC gateway server")
+
+		mux := runtime.NewServeMux()
+
+		// FIXME: Remove hardcoded address
+		opts := []grpc.DialOption{
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		}
+		err := vatzpb.RegisterVatzRPCHandlerFromEndpoint(s.ctx, mux, grpcAddress, opts)
+		if err != nil {
+			log.Info().Str("module", "rpc").Err(err)
+			errChan <- err
+			return
+		}
+
+		s.httpServer = &http.Server{
+			Addr:    httpAddress,
+			Handler: mux,
+		}
+
+		err = s.httpServer.ListenAndServe()
+		if err != nil {
+			log.Info().Str("module", "rpc").Err(err)
+			errChan <- err
+			return
+		}
+	}(errChan)
+
+	err := <-errChan
+
+	log.Info().Str("module", "rpc").Err(err)
+
+	return err
 }
 
 func (s *rpcService) Stop() {
-	s.grpcServer.Stop()
+	log.Info().Str("module", "rpc").Msg("cancel")
+	defer s.cancel()
+
+	if s.httpServer != nil {
+		s.httpServer.Shutdown(s.ctx)
+	}
+
+	log.Info().Str("module", "rpc").Msg("stop")
+	if s.grpcServer != nil {
+		s.grpcServer.Stop()
+	}
 }
 
 // NewRPCService creates new rpc server instance.
 func NewRPCService() VatzRPC {
-	return &rpcService{}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	return &rpcService{
+		ctx:    ctx,
+		cancel: cancel,
+	}
 }
