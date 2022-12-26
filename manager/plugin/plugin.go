9c72df3c626fe52953a4768b62de31f21f801a1b
Joonkyo Kim <rootwarp@gmail.com>
2022-12-26 18:49:50 +0900

    List command of plugin (#335)
    * Impl. list command of plugin
    * Fix typo
    * Fix typo

--- manager/plugin/plugin.go
+++ manager/plugin/plugin.go
@@ -16,9 +16,21 @@ const (
 	pluginDBName = "vatz.db"
 )
 
+// VatzPlugin describes plugin information
+type VatzPlugin struct {
+	ID          string    `json:"id"` // TODO: Handle this after #323
+	Name        string    `json:"name"`
+	Location    string    `json:"location"`
+	Repository  string    `json:"repository"`
+	Version     string    `json:"version"`
+	InstalledAt time.Time `json:"installed_at"`
+}
+
 // VatzPluginManager provides management functions for plugin.
 type VatzPluginManager interface {
 	Install(repo, name, version string) error
+	List() ([]VatzPlugin, error)
+
 	Update() error
 
 	Start(name, args string) error
@@ -86,6 +98,35 @@ func (m *vatzPluginManager) Install(repo, name, version string) error {
 	}
 
 	return nil
+}
+
+func (m *vatzPluginManager) List() ([]VatzPlugin, error) {
+	log.Info().Str("module", "plugin").Msgf("List")
+
+	dbRd, err := newReader(fmt.Sprintf("%s/%s", m.home, pluginDBName))
+	if err != nil {
+		log.Error().Str("module", "plugin").Err(err)
+		return nil, err
+	}
+
+	dbPlugins, err := dbRd.List()
+	if err != nil {
+		log.Error().Str("module", "plugin").Err(err)
+		return nil, err
+	}
+
+	plugins := make([]VatzPlugin, len(dbPlugins))
+
+	for i, p := range dbPlugins {
+		plugins[i].ID = "" // TODO
+		plugins[i].Name = p.Name
+		plugins[i].Repository = p.Repository
+		plugins[i].Location = p.BinaryLocation
+		plugins[i].Version = p.Version
+		plugins[i].InstalledAt = p.InstalledAt
+	}
+
+	return plugins, nil
 }
 
 func (m *vatzPluginManager) Update() error {
