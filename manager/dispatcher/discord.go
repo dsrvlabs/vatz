02eecec5f5463e5f601a8c217040fdf462a44d40
HeeJin Lee <heejin.lee@dsrvlabs.com>
2023-06-02 16:33:17 +0900

    Check the cron entry initialized in dispatcher

--- manager/dispatcher/discord.go
+++ manager/dispatcher/discord.go
@@ -64,6 +64,16 @@ func (d *discord) SetDispatcher(firstRunMsg bool, preStat tp.StateFlag, notifyIn
 		d.entry.Store(pUnique, newEntries)
 		d.reminderCron.Start()
 	} else if reminderState == tp.OFF {
+		count := 0
+		d.entry.Range(func(_, _ interface{}) bool {
+			count++
+			return true
+		})
+		if count == 0 {
+			log.Debug().Str("module", "dispatcher").Msg("There's no entry for the cron.")
+			return nil
+		}
+
 		entries, _ := d.entry.Load(pUnique)
 		for _, entity := range entries.([]cron.EntryID) {
 			{
--- manager/dispatcher/telegram.go
+++ manager/dispatcher/telegram.go
@@ -56,6 +56,16 @@ func (t *telegram) SetDispatcher(firstRunMsg bool, preStat tp.StateFlag, notifyI
 		t.entry.Store(pUnique, newEntries)
 		t.reminderCron.Start()
 	} else if reminderState == tp.OFF {
+		count := 0
+		t.entry.Range(func(_, _ interface{}) bool {
+			count++
+			return true
+		})
+		if count == 0 {
+			log.Debug().Str("module", "dispatcher").Msg("There's no entry for the cron.")
+			return nil
+		}
+
 		entries, _ := t.entry.Load(pUnique)
 		for _, entity := range entries.([]cron.EntryID) {
 			t.reminderCron.Remove(entity)
