405d34ccd972a8d0fb5b96b79017df0bb8e28971
Hwangjae Lee <meetrick@gmail.com>
2024-05-22 20:18:30 +0900

    Modified `vatz_start.sh` scripts (#558)
    Signed-off-by: Hwangjae Lee <meetrick@gmail.com>

--- script/simple_start_guide/vatz_start.sh
+++ script/simple_start_guide/vatz_start.sh
@@ -26,8 +26,8 @@ cd $VATZ_PATH
 
 ./vatz plugin start --plugin node_block_sync --args "-port $NODE_BLOCK_SYNC_PORT" --log $LOG_PATH/node_block_sync.logs
 ./vatz plugin start --plugin node_is_alived --args "-port $NODE_IS_ALIVE_PORT" --log $LOG_PATH/node_is_alived.logs
-./vatz plugin start --plugin node_peer_count --args "-port $NODE_PEER_COUNT" --log $LOG_PATH/node_peer_count.logs
-./vatz plugin start --plugin node_active_status --args "-port $NODE_ACTIVE_STATUS_PORT -valoperAddr $VOTER_ADDRESS" --log $LOG_PATH/node_active_status.logs
+./vatz plugin start --plugin node_peer_count --args "-port $NODE_PEER_COUNT_PORT" --log $LOG_PATH/node_peer_count.logs
+./vatz plugin start --plugin node_active_status --args "-port $NODE_ACTIVE_STATUS_PORT -valoperAddr $VALOPER_ADDRESS" --log $LOG_PATH/node_active_status.logs
 ./vatz plugin start --plugin node_governance_alarm --args "-port $NODE_GOVERNANCE_ALARM_PORT -voterAddr $VOTER_ADDRESS" --log $LOG_PATH/node_governance_alarm.logs
 
 ./vatz start --config default.yaml >> /var/log/vatz/vatz.log 2>&1 &
